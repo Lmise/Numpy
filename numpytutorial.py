2c624f0ee2d6c7f7f0cc85f86cc655a072230d44
Lmise <lucymuhonja@gmail.com>
2018-07-26 17:16:43 +0300

    use arange()

--- numpytutorial.py
+++ numpytutorial.py
@@ -7,6 +7,7 @@ print(a.shape)
 b = np.array([[1, 2, 3], [4, 5, 6]])
 print(b)
 print(b.shape)  # returns (2,3)
+#slicing
 print(b[0, 0], b[0, 1], b[1, 0])  # 1,2,,4
 e = np.random.random((2, 2))
 print(e)
@@ -35,3 +36,7 @@ print(x / y)
 print(np.divide(x, y))
 
 print(np.sqrt(x))
+
+s = np.arange(6)  # works the same as range() in list
+m =np.arange(0,10).reshape(5,2)
+print(m)
